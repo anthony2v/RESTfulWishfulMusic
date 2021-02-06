479ad42c2862821e167d30f1d3e16dbb6699fd39
Tiffany910 <tiffanyahking@gmail.com>
2021-02-06 17:02:45 -0500

    Fixed errors & added AlbumImplementation class, ArtistFactory ,AlbumFactory and working on album rest and artist servlet

--- RepositoryService/src/main/java/com/spicecrispies/Repo_Core/Album.java
+++ RepositoryService/src/main/java/com/spicecrispies/Repo_Core/Album.java
@@ -1,60 +1,56 @@
-package com.spicecrispies.Repo_Base;
+package com.spicecrispies.Repo_Core;
 
 public class Album {
 
     private String isrc;
     private String title;
     private String description;
-    private int year;
+    private int releaseYear;
     private Artist artist;
 
-    public Album(String isrc, String title, String description, int year, Artist artist) {
+    public Album(String isrc, String title, String description, int releaseYear, Artist artist) {
         this.isrc = isrc;
         this.title = title;
         this.description = description;
-        this.year = year;
+        this.releaseYear = releaseYear;
         this.artist = artist;
 
-    }
-
-    public void setIsrc(String isrc) {
-        this.isrc = isrc;
     }
 
     public String getIsrc() {
         return isrc;
     }
 
+    public void setIsrc(String isrc) {
+        this.isrc = isrc;
+    }
+
+    public String getTitle() { return title; }
+
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getDescription() { return description; }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public String getDescription() {
-        return description;
+    public int getReleaseYear() {
+        return releaseYear;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setArtist(Artist artist) {
-        this.artist = artist;
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
     }
 
     public Artist getArtist() {
         return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
     }
 
 
@@ -65,8 +61,9 @@ public class Album {
             description = "No description at ths moment";
         }
         return String.format("ISRC: %s, title: %s, description: %s, release year: %d, artist: %s", isrc, title,
-                description, year, artist);
+                description, releaseYear, artist);
     }
+
 
 }
 
---
+++ RepositoryService/src/main/java/com/spicecrispies/Repo_Implementation/AlbumImplementation.java
@@ -0,0 +1,84 @@
+package com.spicecrispies.Repo_Implementation;
+
+import com.spicecrispies.Repo_Core.Album;
+import com.spicecrispies.Repo_Core.AlbumInterface;
+import com.spicecrispies.Repo_Core.Artist;
+import com.spicecrispies.Repo_Implementation.ArtistImplementation;
+
+public class AlbumImplementation implements AlbumInterface {
+    @Override
+    public String listAlbums() {
+
+        StringBuilder str = new StringBuilder();
+        for(Album album: albums)
+        {
+            str.append(album.toString() + "\n");
+        }
+
+        return str.toString();
+    }
+
+    @Override
+    public String getAlbumDetails(String s) {
+
+        for(Album album: albums)
+        {
+            if(album.getIsrc().equalsIgnoreCase(s))
+            {
+                return album.toString();
+            }
+        }
+
+        return "";
+    }
+
+
+    @Override
+    public boolean addAlbum(String isrc, String title, String description, int releaseYear){
+
+        if(getAlbumDetails(isrc).equalsIgnoreCase("")) {
+            //add album (adds a new album to the collection; no artist details) thats why i put null
+            albums.add(new Album(isrc,title,description, releaseYear,null));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean updateAlbum(String isrc, String title, String description, int releaseYear, Artist artist) {
+        for(Album album: albums)
+        {
+            if(album.getIsrc().equalsIgnoreCase(isrc))
+            {
+                album.setTitle(title);
+                album.setDescription(description);
+                album.setReleaseYear(releaseYear);
+                album.setArtist(artist);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean deleteAlbum(String isrc, String title, String description, int releaseYear) {
+
+        int index = -1;
+        for(
+                Album album: albums)
+        {
+            if(album.getIsrc().equalsIgnoreCase(isrc))
+            {
+                index = albums.indexOf(album);
+            }
+        }
+
+        if(index != -1){
+            albums.remove(index);
+            return true;
+        }
+
+        return false;
+    }
+}
