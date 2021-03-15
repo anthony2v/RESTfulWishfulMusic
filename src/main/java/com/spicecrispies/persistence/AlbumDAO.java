178cc056505da17eeb240af8abab076853f28cc4
anthony2v <anthony_2v@hotmail.com>
2021-03-14 22:17:43 -0400

    Started SQL script for album table

--- src/main/java/com/spicecrispies/persistence/AlbumDAO.java
+++ src/main/java/com/spicecrispies/persistence/AlbumDAO.java
@@ -1,4 +1,10 @@
 package com.spicecrispies.persistence;
 
+import com.spicecrispies.core.entities.Album;
+
 public class AlbumDAO {
+    public Album get(String isrc) {
+        
+    }
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS restful_music;
+USE restful_music;
+
+DROP TABLE IF EXISTS album;
+CREATE TABLE album (
+    isrc VARCHAR(12) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content_description VARCHAR(510),
+    year YEAR NOT NULL,
+    artist_first_name VARCHAR(255) NOT NULL,
+    artist_last_name VARCHAR(255) NOT NULL,
+    cover_image LONGBLOB NOT NULL,
+    cover_image_type VARCHAR(255) NOT NULL
+);
