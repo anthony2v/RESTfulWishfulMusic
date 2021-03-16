231ab492301c2d5d4d22c4ae653d35c0eb02f6b5
Tiffany910 <tiffanyahking@gmail.com>
2021-03-16 18:20:37 -0400

    added localDateTimeAdapter class

--- src/main/java/com/spicecrispies/core/interfaces/LogManagerInterface.java
+++ src/main/java/com/spicecrispies/core/interfaces/LogManagerInterface.java
@@ -3,9 +3,15 @@ package com.spicecrispies.core.interfaces;
 import com.spicecrispies.core.exceptions.RepException;
 import com.spicecrispies.core.logging.LogEntry;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 import java.util.List;
 
+@WebService
+@SOAPBinding
 public interface LogManagerInterface {
+    @WebMethod
     List<LogEntry> getChangeLogs();
     List<LogEntry> getChangeLogs(String fromDate, String toDate, String changeType) throws RepException;
     boolean addLog(LogEntry log) throws RepException;
