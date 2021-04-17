fe530c961f6422b617a82eff7b566ec09b0e5d70
khadija Subtain <khadijaumer786@gmail.com>
2021-04-17 17:36:36 -0400

    DataMapper modification
    DataMapper with DataMapperExample

--- src/main/java/com/spicecrispies/persistence/DataMapperExample.java
+++ src/main/java/com/spicecrispies/persistence/DataMapperExample.java
@@ -1,6 +1,6 @@
 package com.spicecrispies.persistence;
 
-public class DataMapper {
+public class DataMapperExample {
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost/restful_music";
     public static final String USERNAME = "root";
