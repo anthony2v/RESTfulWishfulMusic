4e990d027660c8543735f3106d42dd480917c34f
Khadija Subtain <59979206+khadijaSubtain@users.noreply.github.com>
2021-04-17 17:16:41 -0400

    Updated primary key

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,7 +15,7 @@ CREATE TABLE user (
     id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (name)
+    PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS userAlbum;
